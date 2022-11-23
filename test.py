a222e8eb2ec9f0cf3e2f37e35597e757f5238acb
xc7230 <xc7230@naver.com>
2022-11-23 17:02:50 +0900

    2022.11.23 study

--- test.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-url = 'http://apis.data.go.kr/1790387/covid19CurrentStatusHospitalizations/'
-params ={'serviceKey' : 'mzjIzYfhSW5dqdJTPqPf3RJs2SStFPYtduOgTIRFz28fZZ8iHByfnxVaEtoqiErdfjS0Mo60WUCWNWks9Z3OwQ%3D%3D' }
-
-response = requests.get(url, params=params)
-print(response.content)
