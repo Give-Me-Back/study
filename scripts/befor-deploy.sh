2250de484d6c502720677d7b89aeb897e6c69fd3
AiLEE96 <dlc12233@gmail.com>
2022-12-10 20:48:33 +0900

    test

--- scripts/befor-deploy.sh
+++ scripts/befor-deploy.sh
@@ -4,4 +4,4 @@ if [ -d /app ]; then
 fi
 sudo mkdir -vp /app/
 
-echo test
+echo 2022/12/10
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo apt install nginx -y
 sudo systemctl restart nginx
-echo 2022/12/09 TEST
+echo 2022/12/10
