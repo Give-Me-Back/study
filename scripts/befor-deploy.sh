eed0467a97289aaa22ed236865dd619976c0bdaf
AiLEE96 <dlc12233@gmail.com>
2022-12-11 15:18:33 +0900

    test

--- scripts/befor-deploy.sh
+++ scripts/befor-deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-if [ -d /app ]; then
-    sudo rm -rf /app/
+if [ -d /opt/app ]; then
+    sudo rm -rf /opt/app/
 fi
-sudo mkdir -vp /app/
+sudo mkdir -vp /opt/app/
 
 echo 2022/12/11 TEST
