f1782db8be43343d3ba043a76dc87835bf829d01
AiLEE96 <dlc12233@gmail.com>
2022-12-12 09:39:23 +0900

    test

--- scripts/befor-deploy.sh
+++ scripts/befor-deploy.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-if [ -d /opt/app ]; then
-    sudo rm -rf /app/
-fi
-sudo mkdir -vp /app/
-
-echo 2022/12/11
+echo 2022/12/12
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd /home/ubuntu/GMB/COKO
+echo test
 sudo gunicorn --bind 0.0.0.0:8000 config.wsgi:application
