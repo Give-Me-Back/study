f379ce85f22fa819746fa78d7094fc9323fbbf11
AiLEE96 <dlc12233@gmail.com>
2022-12-12 11:34:20 +0900

    2022/12/12

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /home/ubuntu/GMB/COKO
-echo test
-sudo gunicorn --bind 0.0.0.0:8000 config.wsgi:application
+sudo cd /home/ubuntu/GMB/COKO
+sudo echo test
+sudo nohup gunicorn --bind 0.0.0.0:8000 config.wsgi:application &
