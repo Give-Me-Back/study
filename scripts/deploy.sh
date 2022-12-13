308d6dee0f640a01f7ac5c8b8620a2e98ede3c60
AiLEE96 <dlc12233@gmail.com>
2022-12-13 15:29:41 +0900

    2022/12/13

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-sudo rm -rf /etc/nginx/sites-available/COKO
-sudo rm -rf /etc/nginx/sites-enabled/COKO
+if [ -d /etc/nginx/sites-available/COKO ]; then
+    sudo rm -rf /etc/nginx/sites-available/COKO
+fi
 sudo mv /home/ubuntu/app/scripts/default /etc/nginx/sites-available/
+
+if [ -d /etc/nginx/sites-enabled/COKO ]; then
+    sudo rm -rf /etc/nginx/sites-enabled/COKO
+fi
 sudo ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/
+
 sudo systemctl restart nginx
 
 echo 12/13
